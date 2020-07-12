6a82d084290ee9fafb3b52277e0a58c5f2a14e52
Desperationis <smarttdiego@gmail.com>
2020-07-12 12:03:36 -0700

    Wanted to add more features, but code is too messy. Use of a deprecated PythonFileLibrary

--- main.py
+++ main.py
@@ -1,14 +1,2 @@
-from SettingParser import *
 
-"""
-    main.py
-
-    Converts ROBOT C files to RobotCSimulator compatible ones.
-"""
-
-parser = SettingParser("setup.txt")
-parser.ParseFile()
-
-
-
-
+print('Hello, world!')
