6674de67dd136eb17d7f747c203be77caa121bf8
Desperationis <smarttdiego@gmail.com>
2020-07-12 17:50:29 -0700

    Added pragma conversion

--- FileConverter.py
+++ FileConverter.py
@@ -11,29 +11,62 @@ class FileConverter(Reader):
     def __init__(self, fileName):
         super().__init__(fileName)
 
+        self.convertedFile = [];
+
         try:
             self.Convert()
         except AssertionError as error:
             print(error)
 
-    def GetPragma(self):
+    # Get lines that have '#pragma config' in them. File
+    # can still be naviagted normally.
+    def GetConfig(self):
         for line in self.CleanRead():
-            if '#pragma' in line:
+            if '#pragma' in line and 'config' in line:
                 yield line.strip('\n')
 
         self.ResetReader()
 
+    def ConvertPragma(self):
+
+        arguments = []
+
+        # Get config() arguments
+        for pragma in self.GetConfig():
+            pragma = RemoveElements(pragma, ['#pragma', '(', ')', 'config', ' '])
+            args = pragma.split(',')
+            arguments.append(args)
+
+
+        convertedPragma = []
+        # Step 1: Turn motor and sensor ports into variables.
+        for arg in arguments:
+            type = arg[0]
+            name = arg[2]
+            convertedPragma.append("%sPort %s;\n" % (type, name))
+
+        #Step 2: Use Cortex::config() to configure the ports.
+        convertedPragma.append("void SetUp() {\n")
+        for arg in arguments:
+            type = arg[0]
+            name = arg[2]
+            port = arg[1]
+
+            if type == "Motor":
+
+                reversed = arg[-1] == 'reversed'
+
+                convertedPragma.append("\tCortex::config(\"%s\", %s, %s, %s);\n" % (name, name, port, reversed))
+            else:
+                convertedPragma.append("\tCortex::config(\"%s\", %s, %s);\n" % (name, name, port))
+
+        convertedPragma.append('}\n')
+
+        return convertedPragma
+
+
     def Convert(self):
         assert self.canParse, "FileConverter.py: Could not read \"%s\"" % (self.fileName)
 
-        for pragma in self.GetPragma():
-            pragma = RemoveElements(pragma, ['#pragma', '(', ')'])
-
-            if 'config' in pragma:
-                pragma = pragma.replace('config', "")
-                args = pragma.split(',')
-
-                for i in range(len(args)):
-                    args[i] = args[i].replace(" ", "")
-
-                print(args)
+        for i in self.ConvertPragma():
+            print (i)
